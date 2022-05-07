fdea07024f5d0285c220141fde42eea04657e837
Max <2thousandmax@gmail.com>
2022-05-08 00:43:17 +0300

    remove old files

--- app/components/TaskCard/button.jsx
+++
@@ -1,8 +0,0 @@
-export default function SubmitButton() {
-	return (
-		<button className="bg-zinc-300 hover:bg-gray-400 h-9 text-black ml-4 px-4 rounded font-medium border-1 border-black">
-			Отправить
-		</button>
-
-	)
-}
--- app/routes/scoreboard.jsx
+++
@@ -1,5 +0,0 @@
-export default function Scoreboard() {
-    return (
-        <div>Scoreboard</div>
-    );
-};
