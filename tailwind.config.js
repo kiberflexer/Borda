a3e56f01b46cffd2d142900c5c742a7b6083a5ae
Max <2thousandmax@gmail.com>
2022-05-08 00:42:00 +0300

    styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 30s linear infinite',
+      }
+    },
   },
   plugins: [],
 }
