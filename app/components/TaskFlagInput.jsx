62c4b5ac39bc059dbda29a559e018f70d97c7628
Max <2thousandmax@gmail.com>
2023-12-15 15:45:58 +0300

    update placeholder for event

--- app/components/TaskFlagInput.jsx
+++ app/components/TaskFlagInput.jsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
-import { useFetcher } from '@remix-run/react'
+import {useFetcher} from '@remix-run/react'
 
-import { Button } from '~/components'
-import { Field } from '~/components/Field'
+import {Button} from '~/components'
+import {Field} from '~/components/Field'
 import clsx from 'clsx'
 
-export default function TaskFlagInput({ disabled, className }) {
+export default function TaskFlagInput({disabled, className}) {
     const fetcher = useFetcher()
 
     let inputRef = React.createRef()
@@ -23,7 +23,7 @@ export default function TaskFlagInput({ disabled, className }) {
                 <Field
                     className='m-0'
                     name='flag'
-                    placeholder='STF{s0m3_fl4g}'
+                    placeholder='admctf{s0m3_fl4g}'
                     ref={inputRef}
                     error={fetcher.data?.errors?.flag}
                 />
@@ -33,6 +33,6 @@ export default function TaskFlagInput({ disabled, className }) {
                     className="place-self-end w-auto"
                 />
             </fieldset>
-        </fetcher.Form >
+        </fetcher.Form>
     )
 }
